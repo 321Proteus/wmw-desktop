4d16f90da930cf1a3ea618394966804b0a366822
321Proteus <bgproteus@gmail.com>
2024-04-19 10:51:03 +0200

    Added HTTP view to the example logger

--- example/main.js
+++ example/main.js
@@ -1,7 +1,19 @@
 const { spawn } = require("child_process");
+const express = require("express");
 
+const app = express()
 const wmw = spawn("test.exe");
+
+var text = "";
 
 wmw.stdout.on("data", data => {
     console.log(data.toString().substr(0, data.length-1));
+    text = data.toString();
 })
+
+app.get("/", (req, res) => {
+    console.log(text);
+    res.send(text);
+})
+
+app.listen(5500, () => console.log("Port 5500"));
