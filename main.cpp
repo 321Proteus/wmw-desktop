4c03334590f7e8b3e6adc7df5212e5069e7b74f0
321Proteus <bgproteus@gmail.com>
2024-06-10 20:57:22 +0200

    Added window title data matching

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <regex>
 #include <windows.h>
 
 #include "settings.hpp"
@@ -30,6 +31,46 @@ void formatOutput(string& text, activeWindowData data) {
 
 }
 
+bool getWindowTitleData(string& title, string format, string& file, string& project) {
+
+    string regexString = format;
+    
+    regexString = regex_replace(regexString, regex(R"(%file)"), "(.*?)");
+    regexString = regex_replace(regexString, regex(R"(%project)"), "(.*?)"); 
+
+    cout << regexString << " ";
+
+    smatch matches;
+
+    if (regex_search(title, matches, regex(regexString))) {
+        
+        if (matches.size() == 3) {
+
+            if (format.find("%file") < format.find("%project")) {
+                file = matches[1].str();
+                project = matches[2].str();
+            } else {
+                file = matches[1].str();
+                project = matches[2].str();
+            }
+
+            return true;
+
+        } else if (matches.size() == 2) {
+
+            if (format.find("%file") != string::npos) {
+                file = matches[1].str();
+            } else if (format.find("%project") != string::npos) {
+                project = matches[1].str();
+            }
+
+            return true;
+        }
+    }
+    return false;
+
+}
+
 int main() {
     // HWND hwnd = GetConsoleWindow();
     // ShowWindow(hwnd, SW_HIDE);
@@ -42,21 +83,26 @@ int main() {
         
         HWND target = GetForegroundWindow();
 
+        // Get the window title
+
         char titleBuffer[256];
         GetWindowTextA(target, titleBuffer, 256);
-
         string title(titleBuffer);
+
+        string file, project;
 
         // Acquire the application name, time and PID
         activeWindowData data = fetchWindowInfo(target);
 
+        getWindowTitleData(title, titleFormats[data.pname], file, project);
+        
         string a = format;
         formatOutput(a, data);
 
         /* Check if the PID is not equal to the previously acquired PID
             so the output is only printed when the window changes */
 
-        if (data.name != "") cout << a << endl;
+        if (data.name != "") cout << file << " " << project << endl;
         previousData = data;
 
         Sleep(interval);
